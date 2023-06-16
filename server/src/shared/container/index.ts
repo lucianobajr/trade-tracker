1336997be304b17c86107e7a2786212ec4e6f4fb
lucianobajr <luciano.alcantara@ufv.br>
2023-06-16 10:24:09 -0300

    add(+)

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -2,9 +2,10 @@ import { container } from "tsyringe";
 
 // register singletons
 
-/*
+import { IAdminsRepository } from "../../app/repositories/interfaces/IAdminsRepository"
+import { AdminsRepository } from "../../app/repositories/implementations/AdminsRepository"
+
 container.registerSingleton<IAdminsRepository>(
     "AdminsRepository",
     AdminsRepository
 );
-*/
