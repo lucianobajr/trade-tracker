06b23de0ef8cb776c66a1e41f6543263f2571458
lucianobajr <luciano.alcantara@ufv.br>
2023-06-19 21:25:26 -0300

    add fonts in client

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const Main: React.FC = () => {
-  return <div>
-    <h1 className="font-normal text-4xl text-red-500">
-        Main-2
-    </h1>
-  </div>;
+  return (
+    <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <div className="p-8 bg-white rounded-xl inline-flex flex-col gap-y-12 items-center justify-start">
+        <p className="text-3xl font-medium text-zinc-900">Faça Login na sua Conta</p>
+        <div className="py-4 bg-white flex flex-col gap-y-9 items-center justify-start">
+          <div className="space-y-6">
+
+          </div>
+          <button className=" min-w-full py-3 bg-cyan-600 hover:bg-cyan-700 rounded-2xl font-poppins font-medium text-white transition-colors duration-300">entrar</button>
+          <p className="font-poppins text-xl font-medium leading-tight ">
+            não tem uma conta? <a href="http://" target="_blank" rel="noopener noreferrer" className="text-cyan-600 hover:text-cyan-700 transition-colors duration-300">registre-se</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,28 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px', // Modifique de acordo com suas necessidades
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
+      colors: {
+        'white': '#ffffff',
+        'black': '#000000',
+        'black-secondary': '#1a1c1e',
+        'transparent': '#ffffff',
+        'gray-900': '#1a1c1e',
+        'gray-100': '#f5f4f7',
+        'gray-200': '#e1e0e3',
+        'cyan-600': '#31af99',
+        'cyan-700': '#197d7e',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
